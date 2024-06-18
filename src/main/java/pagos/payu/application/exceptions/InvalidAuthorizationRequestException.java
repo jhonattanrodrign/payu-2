c75415cfd878d8a73f1eeae6614c53d4c7d114e6
jhonattanrodrign <jhonattan.rodriguezn@gmail.com>
2024-06-17 23:57:28 -0500

    exception

--- src/main/java/pagos/payu/application/exceptions/InvalidAuthorizationRequestException.java
+++ src/main/java/pagos/payu/application/exceptions/InvalidAuthorizationRequestException.java
@@ -1,5 +1,8 @@
 package pagos.payu.application.exceptions;
 
+/**
+ * Invalid Authorization Request Exception Handler
+ */
 public class InvalidAuthorizationRequestException extends RuntimeException {
     public InvalidAuthorizationRequestException(String message) {
         super(message);
